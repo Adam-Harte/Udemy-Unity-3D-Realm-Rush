2b48ec238c9a8caa2dbd4e708de895a958090af2
Adam-Harte <adamharte11@hotmail.co.uk>
2024-05-21 23:01:57 +0100

    damaging enemy logic added

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField]
+    int maxHitPoints = 5;
+    int currentHitPoints = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHitPoints = maxHitPoints;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+    }
+
+    void ProcessHit()
+    {
+        currentHitPoints--;
+
+        if (currentHitPoints < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
