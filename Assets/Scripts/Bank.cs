334746e936b047ffc8d91a5d07df0ed90049de05
Adam-Harte <adamharte11@hotmail.co.uk>
2024-05-22 20:02:52 +0100

    UI for currency system balance added

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class Bank : MonoBehaviour
     int startingBalance = 150;
     [SerializeField]
     int currentBalance;
+    [SerializeField]
+    TextMeshProUGUI displayBalance;
     public int CurrentBalance
     {
         get
@@ -17,25 +20,36 @@ public class Bank : MonoBehaviour
         }
     }
 
-    private void Awake() {
+    private void Awake()
+    {
         currentBalance = startingBalance;
+        UpdateDisplay();
     }
 
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
 
-        if (currentBalance < 0) {
+        if (currentBalance < 0)
+        {
             ReloadScene();
         }
     }
 
-    void ReloadScene() {
+    void UpdateDisplay()
+    {
+        displayBalance.text = $"Gold: {currentBalance}";
+    }
+
+    void ReloadScene()
+    {
         Scene currentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(currentScene.buildIndex);
     }
