990ac2d3d9eb4cd58056e7fe3c51fda7812132f9
Adam-Harte <adamharte11@hotmail.co.uk>
2024-05-22 11:16:24 +0100

    enemy movement pathfiding logic updated

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -6,13 +6,33 @@ public class EnemyMover : MonoBehaviour
 {
     [SerializeField]
     List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] [Range(0f, 5f)]
+    [SerializeField]
+    [Range(0f, 5f)]
     float speed = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
+        FindPath();
+        ReturnToStart();
         StartCoroutine(FollowPath());
+    }
+
+    void FindPath()
+    {
+        path.Clear();
+
+        GameObject parent = GameObject.FindGameObjectWithTag("Path");
+
+        foreach (Transform child in parent.transform)
+        {
+            path.Add(child.GetComponent<Waypoint>());
+        }
+    }
+
+    void ReturnToStart()
+    {
+        transform.position = path[0].transform.position;
     }
 
     IEnumerator FollowPath()
@@ -32,5 +52,7 @@ public class EnemyMover : MonoBehaviour
                 yield return new WaitForEndOfFrame();
             }
         }
+
+        Destroy(gameObject);
     }
 }
