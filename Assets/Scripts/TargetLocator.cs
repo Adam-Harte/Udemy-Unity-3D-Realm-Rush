a203720a0d11a90b7c58f2675af975087ec25c90
Adam-Harte <adamharte11@hotmail.co.uk>
2024-05-22 14:28:44 +0100

    enemy targeting and range checking logic added

--- Assets/Scripts/TargetLocator.cs
+++ Assets/Scripts/TargetLocator.cs
@@ -6,22 +6,58 @@ public class TargetLocator : MonoBehaviour
 {
     [SerializeField]
     Transform weapon;
-    
+    [SerializeField]
+    float range = 15f;
+    [SerializeField]
+    ParticleSystem projectileParticles;
+
     Transform target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-    }
 
     // Update is called once per frame
     void Update()
     {
+        FindClosestTarget();
         AimWeapon();
+    }
+
+    void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach (Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if (targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+
+        target = closestTarget;
     }
 
     void AimWeapon()
     {
+        float aimDistance = Vector3.Distance(transform.position, target.transform.position);
         weapon.LookAt(target);
+
+        if (aimDistance <= range)
+        {
+            Attack(true);
+        }
+        else
+        {
+            Attack(false);
+        }
+    }
+
+    void Attack(bool isActive)
+    {
+        var projectile = projectileParticles.emission;
+        projectile.enabled = isActive;
     }
 }
