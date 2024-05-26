de88fd26a89e4d64613b00849e38ee4caf8a70bb
Adam-Harte <adamharte11@hotmail.co.uk>
2024-05-26 21:43:55 +0100

    build time logic added

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,11 +1,17 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
     [SerializeField]
     int cost = 75;
+    [SerializeField]
+    float buildDelay = 0.5f;
+
+    private void Start()
+    {
+        StartCoroutine(Build());
+    }
     public bool CreateTower(Tower tower, Vector3 position)
     {
         Bank bank = FindObjectOfType<Bank>();
@@ -20,5 +26,27 @@ public class Tower : MonoBehaviour
         }
 
         return false;
+    }
+
+    IEnumerator Build()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+            foreach (Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(false);
+            }
+        }
+
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(buildDelay);
+            foreach (Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(true);
+            }
+        }
     }
 }
